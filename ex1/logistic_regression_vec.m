e5eb15efd8e549c803e2671a8e869f03dec5343b
yyli12 <yyli12@fudan.edu.cn>
2016-03-13 20:48:22 +0800

    logistic reg vectorized

--- ex1/logistic_regression_vec.m
+++ ex1/logistic_regression_vec.m
@@ -12,7 +12,12 @@ function [f,g] = logistic_regression_vec(theta, X,y)
   f = 0;
   g = zeros(size(theta));
   
+  h = sigmoid(theta' * X);
+  f = 0 - y * log(h)' - (1-y) * log(1-h)';
+  g = X * (h - y)'; 
 
+  
+  
   %
   % TODO:  Compute the logistic regression objective function and gradient 
   %        using vectorized code.  (It will be just a few lines of code!)
